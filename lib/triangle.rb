208fe77fafc1116cfdff54426e1501cb6cb7f0f2
Cristian <Entenduu@gmail.com>
2022-06-30 19:48:31 -0400

    commit tree 53cd8553fe4c188c47b31fd797f77881ab90b3ba

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @sides=[a,b,c].sort
+    
+  end
+
+  def kind
+    if @sides.any? {|side| side <= 0} || @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError
+    else 
+      case @sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else :scalene
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
 end
